2f034ca320325b7d20f8116bf8d374029cace52c
Dan Adams <danadamsharder@gmail.com>
2021-08-14 15:27:32 -0700

    Finished Running Through Array Problems

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,45 +1,13 @@
 package com.company;
 
+import static com.company.ArraysTesting.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] nums = {12,345,2,6,7896};
-        int potato = RandomTesting.findNumbers(nums);
+        int[] nums1 = {-4,-1,0,3,10};
+
+        System.out.println(sortedSquares(nums1));
     }
-//
-////Binary Search Tree:
-//BinarySearchTree BST = new BinarySearchTree();
-//BSTNode nodeA = new BSTNode(17);
-//BSTNode nodeB = new BSTNode(32);
-//BSTNode nodeC = new BSTNode(10);
-//BSTNode nodeD = new BSTNode(3);
-//BSTNode nodeE = new BSTNode(21);
-//
-//BST.insert(nodeA);
-//BST.insert(nodeB);
-//BST.insert(nodeC);
-//
-//BST.inOrderTraversalPrint(nodeA);
-
-//// LINKED LIST
-//        LinkedList linkedList = new LinkedList();
-//        linkedList.add(1);
-//        linkedList.add(2);
-//        linkedList.add(3);
-////        System.out.println(linkedList.head.data);
-////        System.out.println(linkedList.tail.data);
-////        System.out.println(linkedList.head.next.data);
-//        linkedList.listTraverse(linkedList);
-
-
-
-
-//TWO SUM
-//        int[] nums = new int[] {2, 7,11,15};
-//        int target = 9;
-//
-//        System.out.println(RandomTesting.twoSum(nums, target)[0]);
-//        System.out.println(RandomTesting.twoSum(nums, target)[1]);
-//    }
 }
